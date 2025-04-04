b71551af5bec1d98bfcf51b6988dcab1c8e21284
David Felipe Rios Echeverri <david.rios@thomasgreg.com>
2025-04-03 23:29:35 -0500

    proyecto base

---
+++ src/test/java/co/com/prueba/PruebaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
